35c8528ea77a0320aba2900ff26bee810e047e70
KingRavo18 <rkingovskis14@gmail.com>
2025-10-15 11:03:17 +0300

    d

--- public/dist/script.js
+++ public/dist/script.js
@@ -1,12 +1,24 @@
 function playGame(player_choice) {
     const resultMessage = document.getElementById("result-message");
+    const player_score_display = document.getElementById("player-score");
+    const computer_score_display = document.getElementById("computer-score");
     const computer_options = ["rock", "paper", "scissors"];
     const computer_choice = computer_options[Math.floor(Math.random() * 3)];
     if (player_choice === computer_choice) {
         resultMessage.textContent = "It's a tie!";
         return;
     }
-    resultMessage.textContent = "It's not a tie!";
+    switch (computer_choice) {
+        case "rock":
+            resultMessage.textContent = player_choice === "scissors" ? "You lose. Rock beats scissors." : "You win! Paper beats rock.";
+            break;
+        case "paper":
+            resultMessage.textContent = player_choice === "rock" ? "You lose. Paper beats rock." : "You win! Scissors beat paper.";
+            break;
+        case "scissors":
+            resultMessage.textContent = player_choice === "paper" ? "You lose. Scissors beat paper." : "You win! Rock beats scissors.";
+            break;
+    }
 }
 document.getElementById("rock-btn").onclick = () => playGame('rock');
 document.getElementById("paper-btn").onclick = () => playGame('paper');
--- public/src/script.ts
+++ public/src/script.ts
@@ -1,12 +1,28 @@
 function playGame(player_choice : string) : void {
     const resultMessage: HTMLElement | null = document.getElementById("result-message");
+    const player_score_display: HTMLElement | null = document.getElementById("player-score");
+    const computer_score_display: HTMLElement | null = document.getElementById("computer-score");
     const computer_options: string[] = ["rock", "paper", "scissors"];
     const computer_choice: string | undefined = computer_options[Math.floor(Math.random() * 3)];
+    
+
     if(player_choice === computer_choice){
         (resultMessage as HTMLElement).textContent = "It's a tie!";
         return;
     }
-    (resultMessage as HTMLElement).textContent = "It's not a tie!";
+
+    switch(computer_choice){
+        case "rock":
+            (resultMessage as HTMLElement).textContent = player_choice === "scissors" ? "You lose. Rock beats scissors." : "You win! Paper beats rock.";
+            break;
+        case "paper":
+            (resultMessage as HTMLElement).textContent = player_choice === "rock" ? "You lose. Paper beats rock." : "You win! Scissors beat paper.";
+            break;
+        case "scissors":
+            (resultMessage as HTMLElement).textContent = player_choice === "paper" ? "You lose. Scissors beat paper." : "You win! Rock beats scissors.";
+            break;
+    }
+    
 }
 
 (document.getElementById("rock-btn") as HTMLElement).onclick = () => playGame('rock');
