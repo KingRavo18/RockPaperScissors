5b6ef1ace7aff096e433e99d1920e0097566d724
KingRavo18 <rkingovskis14@gmail.com>
2025-10-15 10:43:43 +0300

    v

--- public/dist/script.js
+++ public/dist/script.js
@@ -1,8 +1,12 @@
 function playGame(player_choice) {
-    const responseContainer = document.getElementById("result-message");
+    const resultMessage = document.getElementById("result-message");
     const computer_options = ["rock", "paper", "scissors"];
     const computer_choice = computer_options[Math.floor(Math.random() * 3)];
-    responseContainer.textContent = "";
+    if (player_choice === computer_choice) {
+        resultMessage.textContent = "It's a tie!";
+        return;
+    }
+    resultMessage.textContent = "It's not a tie!";
 }
 document.getElementById("rock-btn").onclick = () => playGame('rock');
 document.getElementById("paper-btn").onclick = () => playGame('paper');
--- public/src/script.ts
+++ public/src/script.ts
@@ -1,8 +1,12 @@
 function playGame(player_choice : string) : void {
-    const responseContainer: HTMLElement | null = document.getElementById("result-message");
+    const resultMessage: HTMLElement | null = document.getElementById("result-message");
     const computer_options: string[] = ["rock", "paper", "scissors"];
     const computer_choice: string | undefined = computer_options[Math.floor(Math.random() * 3)];
-    (responseContainer as HTMLElement).textContent = "";
+    if(player_choice === computer_choice){
+        (resultMessage as HTMLElement).textContent = "It's a tie!";
+        return;
+    }
+    (resultMessage as HTMLElement).textContent = "It's not a tie!";
 }
 
 (document.getElementById("rock-btn") as HTMLElement).onclick = () => playGame('rock');
